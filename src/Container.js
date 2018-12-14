3cc60b58d096585031de1b33df338401e11319a5
zigoval <rouby.v@hotmail.com>
2018-12-14 20:10:28 +0100

    add loader

--- src/Container.js
+++ src/Container.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import SearchBar from "./SearchBar";
 import ListApi from "./ListApi";
+import { ClipLoader } from "react-spinners";
 
 const url = "https://api.magicthegathering.io/v1/cards/";
 class Container extends Component {
@@ -8,10 +9,12 @@ class Container extends Component {
     super();
     this.state = {
       cards: [],
-      suggestion: []
+      suggestion: [],
+      isLoading: true
     };
   }
   getSuggestions = value => {
+    this.setState(() => ({ isLoading: true }));
     const inputValue = value.trim().toLowerCase();
     const inputLength = inputValue.length;
     const cards = this.state.cards;
@@ -33,6 +36,7 @@ class Container extends Component {
         )
         .catch();
     }
+    this.setState({ isLoading: false });
   };
 
   componentDidMount() {
@@ -40,17 +44,29 @@ class Container extends Component {
       .then(response => response.json())
       .then(data =>
         this.setState(() => ({
-          cards: data.cards
+          cards: data.cards,
+          isLoading: false
         }))
       )
       .catch();
   }
   render() {
     const { cards, suggestion } = this.state;
+    const loading = cards.length === 0 && suggestion.length === 0;
     return (
       <div>
         <SearchBar cards={cards} getSuggestions={this.getSuggestions} />
-        <ListApi cards={cards} suggestion={suggestion} />
+        {loading ? (
+          <ClipLoader
+            className="override"
+            sizeUnit={"px"}
+            size={150}
+            color={"#123abc"}
+            loading={true}
+          />
+        ) : (
+          <ListApi cards={cards} suggestion={suggestion} />
+        )}
       </div>
     );
   }
