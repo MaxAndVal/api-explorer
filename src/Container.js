0931d98a99c5784c7afcb944b35eec3bcb2e2187
zigoval <rouby.v@hotmail.com>
2018-12-16 20:46:57 +0100

    new search bar, seeking using API by name, add button to see more cards, fetching others pages if needs to, add grid for the view

--- src/Container.js
+++ src/Container.js
@@ -2,46 +2,60 @@ import React, { Component } from "react";
 import SearchBar from "./SearchBar";
 import ListApi from "./ListApi";
 import { ClipLoader } from "react-spinners";
+
 import styled from "styled-components";
-import InfiniteScroll from "react-infinite-scroller";
 
 const Grille = styled.div`
   display: grid;
-  grid-template-columns: 200px 200px 200px 200px;
+  grid-template-columns: 250px 250px 250px 250px;
 `;
 
 const url = "https://api.magicthegathering.io/v1/cards/";
+
 class Container extends Component {
   constructor() {
     super();
     this.state = {
       cards: [],
       suggestion: [],
-      isLoading: true
+      isLoading: true,
+      gridSize: 100,
+      page: 1
     };
   }
   getSuggestions = value => {
     this.setState(() => ({ isLoading: true }));
-    const inputValue = value.trim().toLowerCase();
-    const inputLength = inputValue.length;
-    const cards = this.state.cards;
-    const suggestion =
-      inputLength === 0
-        ? []
-        : cards.filter(card => card.name.toLowerCase().slice(0, inputLength) === inputValue);
-    this.setState(state => ({ suggestion: suggestion }));
-    if (suggestion.length < 100) {
-      const urlByName = `https://api.magicthegathering.io/v1/cards?name=${value}`;
-      fetch(urlByName)
-        .then(response => response.json())
-        .then(data =>
+    const urlByName = `https://api.magicthegathering.io/v1/cards?name=${value}`;
+    fetch(urlByName)
+      .then(response => (response.json(), console.log(response.json())))
+      .then(
+        data => (
+          console.log(data),
           this.setState(() => ({
             cards: data.cards
           }))
         )
+      )
+      .catch();
+    this.setState(() => ({ isLoading: false }));
+  };
+
+  increaseSize = () => {
+    let page;
+    this.setState(state => ({ gridSize: state.gridSize + 16 }));
+    if (this.state.gridSize > this.state.cards.length) {
+      this.setState(state => ({ page: state.page + 1 }));
+      page = this.state.page;
+      const urlByPage = `https://api.magicthegathering.io/v1/cards?page=${page}`;
+      fetch(urlByPage)
+        .then(response => response.json())
+        .then(data =>
+          this.setState(state => ({
+            cards: state.cards.concat(data.cards)
+          }))
+        )
         .catch();
     }
-    this.setState({ isLoading: false });
   };
 
   componentDidMount() {
@@ -56,7 +70,7 @@ class Container extends Component {
       .catch();
   }
   render() {
-    const { cards, suggestion } = this.state;
+    const { cards, suggestion, gridSize } = this.state;
     const loading = cards.length === 0 && suggestion.length === 0;
     return (
       <div>
@@ -71,20 +85,10 @@ class Container extends Component {
           />
         ) : (
           <Grille>
-            <InfiniteScroll
-              pageStart={0}
-              loadMore={}
-              hasMore={true || false}
-              loader={
-                <div className="loader" key={0}>
-                  Loading ...
-                </div>
-              }
-            >
-              <ListApi cards={cards} suggestion={suggestion} />
-            </InfiniteScroll>
+            <ListApi cards={cards} gridSize={gridSize} suggestion={suggestion} />
           </Grille>
         )}
+        <button onClick={this.increaseSize}>See More</button>
       </div>
     );
   }
