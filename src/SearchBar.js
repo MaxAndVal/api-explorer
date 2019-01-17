3fc83729f2e6a1911af357add97d8a0c7603da52
Zigoval <rouby.v@hotmail.com>
2019-01-17 14:25:24 +0100

    feat(searchBar) material design and fix bug on subtype and add supertype filter + this is not the droids

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -9,11 +9,27 @@ import Input from "@material-ui/core/Input";
 const SearchBar = props => {
   const listTypes = props.types || [];
   const listSubTypes = props.subtypes || [];
-
+  const listSupertype = props.supertypes || [];
   return (
-    <form>
-      <input placeholder="Search for..." onChange={e => props.getSuggestions(e.target.value)} />
-      <Select onChange={e => props.selectAType(e.target.value)} name="type">
+    <form
+      style={{
+        display: "flex",
+        justifyContent: "space-around",
+        paddingBottom: "25px",
+        marginTop: "25px",
+        borderBottom: "1px solid black"
+      }}
+    >
+      <Input
+        placeholder="Search for..."
+        onChange={e => props.getSuggestions(e.target.value)}
+      />
+      <InputLabel>Type</InputLabel>
+      <Select
+        onChange={e => props.selectAType(e.target.value)}
+        name="type"
+        value={props.selectedType}
+      >
         <MenuItem value="">tous</MenuItem>
         {listTypes.map((type, key) => (
           <MenuItem value={type} key={key}>
@@ -21,14 +37,29 @@ const SearchBar = props => {
           </MenuItem>
         ))}
       </Select>
+      <InputLabel>Subtype</InputLabel>
       <Select
         onChange={e => props.selectASubtype(e.target.value)}
-        input={<Input name="age" id="age-helper" />}
+        name="subtype"
+        value={props.selectedSubtype}
       >
         <MenuItem value="">tous</MenuItem>
         {listSubTypes.map((subtype, key) => (
           <MenuItem value={subtype} key={key}>
             {subtype}
+          </MenuItem>
+        ))}
+      </Select>
+      <InputLabel>Supertype</InputLabel>
+      <Select
+        onChange={e => props.selectASupertype(e.target.value)}
+        name="Supertype"
+        value={props.selectedSupertype}
+      >
+        <MenuItem value="">tous</MenuItem>
+        {listSupertype.map((supertype, key) => (
+          <MenuItem value={supertype} key={key}>
+            {supertype}
           </MenuItem>
         ))}
       </Select>
