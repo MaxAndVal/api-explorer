93ef8148583b73aba796f3a1f4beacd096f00837
Zigoval <rouby.v@hotmail.com>
2019-01-17 14:52:04 +0100

    feat/(404) image is centered

--- src/404.js
+++ src/404.js
@@ -2,10 +2,12 @@ import React from "react";
 
 const Error404 = () => {
   return (
-    <img
-      src={require("./images/DeadLink.jpg")}
-      alt="Error 404, page not found"
-    />
+    <div style={{ textAlign: "center" }}>
+      <img
+        src={require("./images/DeadLink.jpg")}
+        alt="Error 404, page not found"
+      />
+    </div>
   );
 };
 
