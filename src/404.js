5bef55259d56b407577f9f7043a3f6289709aec2
Zigoval <rouby.v@hotmail.com>
2019-01-17 14:50:47 +0100

    feat/error404 redirect

---
+++ src/404.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Error404 = () => {
+  return (
+    <img
+      src={require("./images/DeadLink.jpg")}
+      alt="Error 404, page not found"
+    />
+  );
+};
+
+export default Error404;
