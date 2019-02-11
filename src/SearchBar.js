d8e44ed4ce1845cb0fa19fcb8ae56f6550cc26de
MALOSSE MAXIME p1713335 <maxime.malosse@etu.univ-lyon1.fr>
2019-02-11 10:14:42 +0100

    fix merge

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,15 +1,8 @@
 import React from "react";
 
-<<<<<<< HEAD
-class SearchBar extends Component {
-  state = {
-    value: "" 
-  };
-=======
 const SearchBar = props => {
   const listTypes = props.types || [];
   const listSubTypes = props.subtypes || [];
->>>>>>> master
 
   return (
     <form>
