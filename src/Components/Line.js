1db96bc50a35ba37ce0c961b7ffcf0cc4ea4e37a
MALOSSE MAXIME p1713335 <maxime.malosse@etu.univ-lyon1.fr>
2019-02-12 10:12:37 +0100

    feat/ added css

--- src/Components/Line.js
+++ src/Components/Line.js
@@ -4,7 +4,7 @@ import { DivResult, DivSpec } from "../Styles";
 const Line = (props) => {
     return(
         <div style={{ display: "flex" }}>
-            <DivSpec>{props.type}</DivSpec>
+            <DivSpec style={{textTransform: "uppercase", fontStyle: "italic", fontFamily: "fantasy"}}>{props.type}</DivSpec>
             <DivResult> {props.value}</DivResult>
           </div>
     );
